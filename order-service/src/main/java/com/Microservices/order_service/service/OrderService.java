5ecf528e957b5d3b16a0d323b5d2b680200469c3
Hitarthpancha121 <Hitarth9950@gmail.com>
2025-04-04 12:55:51 +0530

    Updated Commit

--- order-service/src/main/java/com/Microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/Microservices/order_service/service/OrderService.java
@@ -20,7 +20,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrders(OrderRequest orderRequest){
         Order order=new Order();
@@ -37,8 +37,8 @@ public class OrderService {
            .toList();
 
    // call inventory service , and place order only if the product is in the stock
-       InventoryResponse[] inventoryResponsesArray= webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+       InventoryResponse[] inventoryResponsesArray= webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
                         .retrieve()
                                 .bodyToMono(InventoryResponse[].class)
